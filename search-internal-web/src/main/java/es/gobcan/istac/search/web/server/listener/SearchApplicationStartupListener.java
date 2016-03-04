a69ca2904f6b6e30b8af0dee4e878d31a6687d31
frodgar <frodgar@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2016-03-04 12:49:51 +0000

    BUSCAISTAC-27  - Crear aplicativo web de gestión que sustituya al idxmanager > Añadidas comprobaciones sobre properties de configuracion
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/search/trunk/search@54922 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- search-internal-web/src/main/java/es/gobcan/istac/search/web/server/listener/SearchApplicationStartupListener.java
+++ search-internal-web/src/main/java/es/gobcan/istac/search/web/server/listener/SearchApplicationStartupListener.java
@@ -12,6 +12,31 @@ public class SearchApplicationStartupListener extends InternalApplicationStartup
         checkRequiredProperty(SearchConfigurationConstants.DB_SEARCH_DRIVER_NAME);
         checkRequiredProperty(SearchConfigurationConstants.DB_SEARCH_USERNAME);
         checkRequiredProperty(SearchConfigurationConstants.DB_SEARCH_PASSWORD);
+
+        checkRequiredPropertiesSolr();
+        checkRequiredPropertiesAlfresco();
+        checkRequiredPropertiesIndexation();
+    }
+
+    private void checkRequiredPropertiesSolr() {
+        checkRequiredProperty(SearchConfigurationConstants.SEARCH_SOLR_JAXI_URL);
+        checkRequiredProperty(SearchConfigurationConstants.SEARCH_SOLR_ENDPOINT);
+        checkRequiredProperty(SearchConfigurationConstants.SEARCH_SOLR_CORE_OR_COLLECTION);
+        checkRequiredProperty(SearchConfigurationConstants.SEARCH_SOLR_CLOUD_SERVER_ENABLED);
+    }
+
+    private void checkRequiredPropertiesAlfresco() {
+        checkRequiredProperty(SearchConfigurationConstants.SEARCH_ALFRESCO_URL);
+        checkRequiredProperty(SearchConfigurationConstants.SEARCH_ALFRESCO_USERNAME);
+        checkRequiredProperty(SearchConfigurationConstants.SEARCH_ALFRESCO_PASSWORD);
+        checkRequiredProperty(SearchConfigurationConstants.SEARCH_ALFRESCO_PATH);
+    }
+
+    private void checkRequiredPropertiesIndexation() {
+        checkRequiredProperty(SearchConfigurationConstants.SEARCH_INDEXATION_WEB_CRON);
+        checkRequiredProperty(SearchConfigurationConstants.SEARCH_INDEXATION_WEB_URL);
+        checkRequiredProperty(SearchConfigurationConstants.SEARCH_INDEXATION_GPE_CRON);
+        checkRequiredProperty(SearchConfigurationConstants.SEARCH_INDEXATION_RECOMMENDED_LINKS);
     }
 
     @Override
