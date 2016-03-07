5205e5a52cc74d940eeb8e2a355d9b4cfebe181d
frodgar <frodgar@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2016-03-07 08:59:07 +0000

    BUSCAISTAC-27  - Crear aplicativo web de gestión que sustituya al idxmanager > Copiados servlets gpeServlet y quartzInitializer desde el antiguo idxmanager-web
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/search/trunk/search@54939 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ search-internal-web/src/main/java/es/gobcan/istac/search/web/server/servlet/GpeServlet.java
@@ -0,0 +1,108 @@
+package es.gobcan.istac.idxmanager.web.servlet;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.thoughtworks.xstream.XStream;
+
+import es.gobcan.istac.idxmanager.service.excepcion.ServiceExcepcion;
+import es.gobcan.istac.idxmanager.service.nucleoistac.NucleoIstacIndexerService;
+import es.gobcan.istac.idxmanager.service.util.ApplicationContextProvider;
+import es.gobcan.istac.jaxi.pxservice.api.dominio.NucleoMetadatos;
+
+
+/**
+ * Clase que recibe un HttpPost con una serializacion del bean del nucleo de metadatos del istac.
+ * 
+ * @author arte
+ *
+ */
+public class GpeServlet extends HttpServlet {
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        boolean hayError = false;
+        
+        /*
+         * Deserializacion del objeto enviado
+         */
+        String direccion = req.getHeader("direccion");
+        if (direccion.equals("entrada")) {
+            XStream xstream = new XStream();
+            ObjectInputStream in = xstream.createObjectInputStream(req.getInputStream());
+            NucleoMetadatos nucleoMetadatos = null;
+            
+            
+            try {
+                nucleoMetadatos = (NucleoMetadatos) in.readObject();
+            } catch (ClassNotFoundException e) {
+                hayError = true;
+            }
+
+            /*
+             * Indexacion del objeto enviado
+             */
+            
+            if (!hayError) {
+                NucleoIstacIndexerService nucleoIstacIndexerService = (NucleoIstacIndexerService) ApplicationContextProvider.getApplicationContext().getBean("nucleoIstacIndexerServiceImpl");
+                
+                try {
+                    nucleoIstacIndexerService.indexarElementoGPE(nucleoMetadatos, req.getHeader("formatoRecurso"));
+                    nucleoIstacIndexerService.commitANDoptimize();
+                } catch (ServiceExcepcion e) {
+                    hayError = true;
+                }
+            }
+        } else if (direccion.equals("salida")) {
+            ObjectInputStream in = new ObjectInputStream(req.getInputStream());
+            String id = null;
+            
+            try {
+                id = (String) in.readObject();
+            } catch (ClassNotFoundException e) {
+                hayError = true;
+            }
+            
+            /*
+             * DesIndexacion del objeto enviado
+             */
+            
+            
+            NucleoIstacIndexerService nucleoIstacIndexerService = (NucleoIstacIndexerService) ApplicationContextProvider.getApplicationContext().getBean("nucleoIstacIndexerServiceImpl");
+            
+            try {
+                nucleoIstacIndexerService.eliminarElemento(id);
+                nucleoIstacIndexerService.commitANDoptimize();
+            } catch (ServiceExcepcion e) {
+                hayError = true;
+            }
+        }
+
+        
+        /*
+         * Respuesta satisfactoria/insatisfactoria
+         */
+
+        resp.setContentType("text/html");
+        if (!hayError) {
+            resp.setStatus(resp.SC_ACCEPTED);
+        }
+        else {
+            resp.setStatus(resp.SC_INTERNAL_SERVER_ERROR);
+        }
+        
+
+    }
+}
---
+++ search-internal-web/src/main/java/es/gobcan/istac/search/web/server/servlet/QuartzInitializerListener.java
@@ -0,0 +1,219 @@
+package es.gobcan.istac.idxmanager.web.servlet;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.quartz.CronTrigger;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.impl.StdSchedulerFactory;
+
+import com.arte.acom.configuration.ConfigurationService;
+import com.arte.acom.configuration.ConfigurationServiceImpl;
+
+import es.gobcan.istac.idxmanager.service.indexacion.CrawlerJob;
+import es.gobcan.istac.idxmanager.service.indexacion.GPEIndexerJob;
+import es.gobcan.istac.idxmanager.service.util.ApplicationContextProvider;
+
+/**
+ * <p>
+ * A ServletContextListner that can be used to initialize Quartz.
+ * </p>
+ * <p>
+ * You'll want to add something like this to your WEB-INF/web.xml file:
+ *
+ * <pre>
+ *     &lt;context-param&gt;
+ *         &lt;param-name&gt;quartz-config-name&lt;/param-name&gt;
+ *         &lt;param-value&gt;quartz&lt;/param-value&gt;
+ *     &lt;/context-param&gt;
+ *     &lt;context-param&gt;
+ *         &lt;param-name&gt;quartz-shutdown-on-unload&lt;/param-name&gt;
+ *         &lt;param-value&gt;true&lt;/param-value&gt;
+ *     &lt;/context-param&gt;
+ *     &lt;context-param&gt;
+ *         &lt;param-name&gt;quartz-start-scheduler-on-load&lt;/param-name&gt;
+ *         &lt;param-value&gt;true&lt;/param-value&gt;
+ *     &lt;/context-param&gt;
+ *     
+ *     &lt;listener&gt;
+ *         &lt;listener-class&gt;
+ *             es.gobcan.istac.gpe.web.servlet.QuartzInitializerListener
+ *         &lt;/listener-class&gt;
+ *     &lt;/listener&gt;
+ * </pre>
+ *
+ * </p>
+ * <p>
+ * The init parameter 'quartz-config-name' can be used to specify the name. If you leave out this parameter, the default ("quartz") will be used.
+ * </p>
+ * <p>
+ * The init parameter 'shutdown-on-unload' can be used to specify whether you want scheduler.shutdown() called when the servlet is unloaded (usually when the application server is being shutdown).
+ * Possible values are "true" or "false". The default is "true".
+ * </p>
+ * <p>
+ * The init parameter 'start-scheduler-on-load' can be used to specify whether you want the scheduler.start() method called when the servlet is first loaded. If set to false, your application will
+ * need to call the start() method before the scheduler begins to run and process jobs. Possible values are "true" or "false". The default is "true", which means the scheduler is started.
+ * </p>
+ * A StdSchedulerFactory instance is stored into the ServletContext. You can gain access
+ * to the factory from a ServletContext instance like this: <br>
+ * 
+ * <pre>
+ * 
+ * StdSchedulerFactory factory = (StdSchedulerFactory) ctx.getAttribute(QuartzInitializerListener.QUARTZ_FACTORY_KEY);
+ * </pre>
+ * <p>
+ * The init parameter 'servlet-context-factory-key' can be used to override the name under which the StdSchedulerFactory is stored into the ServletContext, in which case you will want to use this name
+ * rather than <code>QuartzInitializerListener.QUARTZ_FACTORY_KEY</code> in the above example.
+ * </p>
+ * <p>
+ * The init parameter 'quartz-start-delay-seconds' can be used to specify the amount of time to wait after initializing the scheduler before scheduler.start() is called.
+ * </p>
+ * Once you have the factory instance, you can retrieve the Scheduler instance by calling <code>getScheduler()</code> on the factory.
+ *
+ * @author James House
+ * @author Chuck Cavaness
+ * @author John Petrocik
+ */
+public class QuartzInitializerListener implements ServletContextListener {
+
+    protected static Log LOG = LogFactory.getLog(QuartzInitializerListener.class);
+
+    public static final String QUARTZ_FACTORY_KEY = "org.quartz.impl.StdSchedulerFactory.KEY";
+
+    private boolean performShutdown = true;
+
+    private Scheduler scheduler = null;
+
+    private ConfigurationService configurationService;
+
+    /*
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * Interface.
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
+
+    public void contextInitialized(ServletContextEvent sce) {
+
+        System.out.println("Quartz Initializer Servlet loaded, initializing Scheduler...");
+
+        ServletContext servletContext = sce.getServletContext();
+        StdSchedulerFactory factory;
+        try {
+            // String configName = servletContext.getInitParameter("quartz-config-name");
+            // if (configName == null) {
+            // configName = "quartz";
+            // }
+            String shutdownPref = servletContext.getInitParameter("quartz-shutdown-on-unload");
+
+            configurationService = (ConfigurationService) ApplicationContextProvider.getApplicationContext().getBean("configurationService");
+            Properties quartzProperties = configurationService.getProperties();
+
+            if (shutdownPref != null) {
+                performShutdown = Boolean.valueOf(shutdownPref).booleanValue();
+            }
+
+            // get Properties
+            factory = new StdSchedulerFactory(quartzProperties);
+
+            // Always want to get the scheduler, even if it isn't starting,
+            // to make sure it is both initialized and registered.
+            scheduler = factory.getScheduler();
+
+            // Should the Scheduler being started now or later
+            String startOnLoad = servletContext.getInitParameter("quartz-start-scheduler-on-load");
+
+            int startDelay = 0;
+            String startDelayS = servletContext.getInitParameter("quartz-start-delay-seconds");
+            try {
+                if (startDelayS != null && startDelayS.trim().length() > 0) {
+                    startDelay = Integer.parseInt(startDelayS);
+                }
+            } catch (Exception e) {
+                System.out.println("Cannot parse value of 'start-delay-seconds' to an integer: " + startDelayS + ", defaulting to 5 seconds.");
+                startDelay = 5;
+            }
+
+            // Crea Jobs CRON
+            crearJobsCron();
+
+            /*
+             * If the "start-scheduler-on-load" init-parameter is not specified,
+             * the scheduler will be started. This is to maintain backwards
+             * compatability.
+             */
+            if (startOnLoad == null || (Boolean.valueOf(startOnLoad).booleanValue())) {
+                if (startDelay <= 0) {
+                    // Start now
+                    scheduler.start();
+                    System.out.println("Scheduler has been started...");
+                } else {
+                    // Start delayed
+                    scheduler.startDelayed(startDelay);
+                    System.out.println("Scheduler will start in " + startDelay + " seconds.");
+                }
+            } else {
+                System.out.println("Scheduler has not been started. Use scheduler.start()");
+            }
+
+            String factoryKey = servletContext.getInitParameter("quartz-servlet-context-factory-key");
+            if (factoryKey == null) {
+                factoryKey = QUARTZ_FACTORY_KEY;
+            }
+
+            System.out.println("Storing the Quartz Scheduler Factory in the servlet context at key: " + factoryKey);
+            LOG.info("QuartzInitializerListener.contextInitialized --> Guardada la inicializacion del quartz bajo la clave " + factoryKey);
+
+            servletContext.setAttribute(factoryKey, factory);
+
+        } catch (Exception e) {
+            System.out.println("Quartz Scheduler failed to initialize: " + e.toString());
+            LOG.error("Fallo en la inicializacion del quartz --> QuartzInitializerListener.contextInitialized :: ", e);
+        }
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+
+        if (!performShutdown) {
+            return;
+        }
+
+        try {
+            if (scheduler != null) {
+                scheduler.shutdown();
+            }
+        } catch (Exception e) {
+            System.out.println("Quartz Scheduler failed to shutdown cleanly: " + e.toString());
+            LOG.error("Fallo en la destruccion del quartz --> QuartzInitializerListener.contextDestroyed :: ", e);
+        }
+
+        LOG.info("QuartzInitializerListener.contextDestroyed --> La finalizacion del quatz ha sido correcta");
+        System.out.println("Quartz Scheduler successful shutdown.");
+    }
+
+    private void crearJobsCron() throws Exception {
+        // INDEXACION WEB JOB
+        JobDetail job = new JobDetail("crawlerJOB", "groupIdx", CrawlerJob.class);
+
+        Properties properties = ((ConfigurationServiceImpl) ApplicationContextProvider.getApplicationContext().getBean("configurationService")).getProperties();
+        CronTrigger trigger = new CronTrigger("triggerCrawler", "groupIdx", "crawlerJOB", "groupIdx", (String) properties.get("istac.idxmanager.indexacion.web.cron"));
+        scheduler.addJob(job, true);
+        Date ft = scheduler.scheduleJob(trigger);
+        LOG.info(job.getFullName() + " ha sido planificado para ejecutase en fecha: " + ft + " y repetirse de acuerdo a la " + " expresion cron: " + trigger.getCronExpression());
+
+        job = new JobDetail("gpeIndexerJOB", "groupIdx", GPEIndexerJob.class);
+        trigger = new CronTrigger("triggerGPEIndexer", "groupIdx", "gpeIndexerJOB", "groupIdx", (String) properties.get("istac.idxmanager.indexacion.gpe.cron"));
+        scheduler.addJob(job, true);
+
+        ft = scheduler.scheduleJob(trigger);
+        LOG.info(job.getFullName() + " ha sido planificado para ejecutase en fecha: " + ft + " y repetirse de acuerdo a la " + " expresion cron: " + trigger.getCronExpression());
+
+    }
+
+}
