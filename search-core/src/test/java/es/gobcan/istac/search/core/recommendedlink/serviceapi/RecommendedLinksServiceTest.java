938dddb75a7303de965abb2d3f51e7176a6db38a
frodgar <frodgar@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2016-04-11 08:54:10 +0000

    BUSCAISTAC-28 - Permitir modificar la compra de términos a través del aplicativo de gestión >Corrección tests
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/search/trunk/search@55118 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- search-core/src/test/java/es/gobcan/istac/search/core/recommendedlink/serviceapi/RecommendedLinksServiceTest.java
+++ search-core/src/test/java/es/gobcan/istac/search/core/recommendedlink/serviceapi/RecommendedLinksServiceTest.java
@@ -1,61 +1,60 @@
-// package es.gobcan.istac.search.core.recommendedlink.serviceapi;
-//
+package es.gobcan.istac.search.core.recommendedlink.serviceapi;
+
 // import org.fornax.cartridges.sculptor.framework.test.AbstractDbUnitJpaTests;
-// import org.junit.Test;
-//
-/// **
-// * Spring based transactional test with DbUnit support.
-// */
-// public class RecommendedLinksServiceTest extends AbstractDbUnitJpaTests implements RecommendedLinksServiceTestBase {
-// //
-// // @Autowired
-// // protected RecommendedLinksService recommendedLinksService;
-//
-// @Override
-// @Test
-// public void testFindRecommendedLinkById() throws Exception {
-// // TODO Auto-generated method stub
-// // fail("testFindRecommendedLinkById not implemented");
-// }
-//
-// @Override
-// @Test
-// public void testCreateRecommendedLink() throws Exception {
-// // TODO Auto-generated method stub
-// // fail("testCreateRecommendedLink not implemented");
-// }
-//
-// @Override
-// @Test
-// public void testUpdateRecommendedLink() throws Exception {
-// // TODO Auto-generated method stub
-// // fail("testUpdateRecommendedLink not implemented");
-// }
-//
-// @Override
-// @Test
-// public void testDeleteRecommendedLink() throws Exception {
-// // TODO Auto-generated method stub
-// // fail("testDeleteRecommendedLink not implemented");
-// }
-//
-// @Override
-// @Test
-// public void testFindAllRecommendedLinks() throws Exception {
-// // TODO Auto-generated method stub
-// // fail("testFindAllRecommendedLinks not implemented");
-// }
-//
-// @Override
-// @Test
-// public void testFindRecommendedLinksByCondition() throws Exception {
-// // TODO Auto-generated method stub
-// // fail("testFindRecommendedLinksByCondition not implemented");
-// }
-//
-// @Override
-// public void testExportRecommendedLinks() throws Exception {
-// // TODO Auto-generated method stub
-// // fail("testExportRecommendedLinks not implemented");
-// }
-// }
+import org.junit.Test;
+
+/*** Spring based transactional test with DbUnit support. */
+
+public class RecommendedLinksServiceTest /* extends AbstractDbUnitJpaTests */ implements RecommendedLinksServiceTestBase {
+    //
+    // @Autowired
+    // protected RecommendedLinksService recommendedLinksService;
+
+    @Override
+    @Test
+    public void testFindRecommendedLinkById() throws Exception {
+        // TODO Auto-generated method stub
+        // fail("testFindRecommendedLinkById not implemented");
+    }
+
+    @Override
+    @Test
+    public void testCreateRecommendedLink() throws Exception {
+        // TODO Auto-generated method stub
+        // fail("testCreateRecommendedLink not implemented");
+    }
+
+    @Override
+    @Test
+    public void testUpdateRecommendedLink() throws Exception {
+        // TODO Auto-generated method stub
+        // fail("testUpdateRecommendedLink not implemented");
+    }
+
+    @Override
+    @Test
+    public void testDeleteRecommendedLink() throws Exception {
+        // TODO Auto-generated method stub
+        // fail("testDeleteRecommendedLink not implemented");
+    }
+
+    @Override
+    @Test
+    public void testFindAllRecommendedLinks() throws Exception {
+        // TODO Auto-generated method stub
+        // fail("testFindAllRecommendedLinks not implemented");
+    }
+
+    @Override
+    @Test
+    public void testFindRecommendedLinksByCondition() throws Exception {
+        // TODO Auto-generated method stub
+        // fail("testFindRecommendedLinksByCondition not implemented");
+    }
+
+    @Override
+    public void testExportRecommendedLinks() throws Exception {
+        // TODO Auto-generated method stub
+        // fail("testExportRecommendedLinks not implemented");
+    }
+}
