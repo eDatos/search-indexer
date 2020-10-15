f2a600953c7b9658c516027f2c623b1a44e4c3e0
Laura M. Gutiérrez Medina <lgutmed@arte-consultores.com>
2020-10-15 09:19:24 +0100

    EDATOS-3197 - Compatibilidad con PostgreSQL

---
+++ etc/db/search-indexer/postgresql/02-drop/01-search-schema-drop.sql
@@ -0,0 +1,30 @@
+    
+-- ###########################################
+-- # Drop
+-- ###########################################
+-- Drop index
+
+
+-- Drop many to many relations
+    
+-- Drop normal entities
+    
+        
+DROP TABLE TB_RECOMMENDED_LINKS CASCADE;
+
+    	
+DROP TABLE TB_RECOMMENDED_KEYWORDS CASCADE;
+
+    	
+DROP TABLE TB_LOCALISED_STRINGS CASCADE;
+
+    	
+DROP TABLE TB_EXTERNAL_ITEMS CASCADE;
+
+    	
+DROP TABLE TB_INTERNATIONAL_STRINGS CASCADE;
+
+
+-- Drop pk sequence
+    
+DROP TABLE TB_SEQUENCES;
